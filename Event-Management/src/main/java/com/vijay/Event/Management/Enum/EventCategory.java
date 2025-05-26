289fa8e668a74c1eb0b17b491df6f5ea086f893c
vijaybadgurjar <vijaybhdgujjar@gmail.com>
2025-05-27 01:02:29 +0530

    Event model and Enum added

---
+++ Event-Management/src/main/java/com/vijay/Event/Management/Enum/EventCategory.java
@@ -0,0 +1,27 @@
+package com.vijay.Event.Management.Enum;
+
+public enum EventCategory {
+    CONCERT,
+    CONFERENCE,
+    MUSIC,
+    SPORTS,
+    ART,
+    EDUCATION,
+    ENTERTAINMENT,
+    FOOD,
+    TECHNOLOGY,
+    TRAVEL,
+    CHARITY,
+    BUSINESS,
+    WORKSHOP,
+    SEMINAR,
+    FESTIVAL,
+    EXHIBITION,
+    THEATER,
+    NETWORKING,
+    PARTY,
+    COMPETITION,
+    FUNDRAISER,
+    WEBINAR,
+    OTHER
+}
---
+++ Event-Management/src/main/java/com/vijay/Event/Management/Enum/EventStatus.java
@@ -0,0 +1,10 @@
+package com.vijay.Event.Management.Enum;
+
+public enum EventStatus {
+
+    UPCOMING,
+    ONGOING,
+    COMPLETED,
+    RESCHEDULED,
+    CANCELLED;
+}
